62ae31e12fde3c01cb495537de020f580176557d
Max Gerun <maxgerun@gmail.com>
2022-10-14 17:36:28 +0300

    routes ad crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  post '/questions', to: 'questions#create'
+  # post '/questions', to: 'questions#create'
+  # patch '/questions/:id', to: 'questions#update'
+  # delete '/questions/:id', to: 'questions#destroy'
+
+  resources :questions
 end

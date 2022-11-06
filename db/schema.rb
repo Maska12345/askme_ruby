1d931b0b79398a3a29dc7233b3298be5bf97f778
Max Gerun <maxgerun@gmail.com>
2022-11-06 22:03:53 +0200

    authenticate,cookies,session

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_05_195321) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_06_195256) do
   create_table "questions", force: :cascade do |t|
     t.text "body"
     t.integer "user_id"
@@ -25,6 +25,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_05_195321) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "passwords_digest"
+    t.string "password_digest"
   end
 
 end
